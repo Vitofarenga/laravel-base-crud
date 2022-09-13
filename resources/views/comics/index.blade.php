63715edef1d47c67a66d1d0dee2a334bf600508d
Vito Farenga <vitofarenga@libero.it>
2022-09-13 16:51:57 +0200

    table e seeder

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Main Comics')
+
+@section('main-content')
+         <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+
+                </div>
+
+            </div>
+         </div>
+@endsection
