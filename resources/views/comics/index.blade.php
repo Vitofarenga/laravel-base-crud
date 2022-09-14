09cee5f568072be9ceaa5f082d2122e908af4c89
Vito Farenga <vitofarenga@libero.it>
2022-09-14 16:24:47 +0200

    route fatta, creazione tabella

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,11 +3,24 @@
 @section('title', 'Main Comics')
 
 @section('main-content')
-         <div class="container-fluid">
+         <div class="container my-5">
             <div class="row">
-                <div class="col-12">
-
-                </div>
+                
+                   <table class="table table-striped">
+                    <thead>
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                        <th>4</th>
+                       </thead>
+                       <tbody>
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                        <th>4</th>
+                       </tbody>
+                   </table>
+                
 
             </div>
          </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('admin.home');
+Route::get('/', 'ComicController@index')->name('comics.index');
